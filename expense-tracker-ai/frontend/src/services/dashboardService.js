78072a0b5068a1b109ef39a0ee1a1813c8ad3136
reash-08 <reashmar01@gmail.com>
2026-08-19 20:06:23 +0530

    Delete dashboardService.js

--- expense-tracker-ai/frontend/src/services/dashboardService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const dashboardService = {
-  async getDashboardSummary(params = {}) {
-    const response = await api.get('/dashboard/', { params });
-    return response.data?.data;
-  },
-};
